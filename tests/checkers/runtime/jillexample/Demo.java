ccc9fa49a982cf014957cf59c4eae4e1df6e974b
Werner Dietl <wdietl@gmail.com>
2011-02-16 15:10:33 -0800

    Playing around.

--- tests/checkers/runtime/jillexample/Demo.java
+++ tests/checkers/runtime/jillexample/Demo.java
@@ -1,8 +1,10 @@
 package checkers.runtime.jillexample;
 
 public class Demo {
-  static Object o = "a String!";
+  static Object o = (String) "a String!";
 
   public static void main(String[] args) {
+      // Object o = (String) "a String!";
+      // String s = (String) o;
   }
 }
