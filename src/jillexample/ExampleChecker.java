fbc002690f6b9d7865a1bd2b252cb2cd56e47ab0
Werner Dietl <wdietl@gmail.com>
2011-02-16 12:07:48 -0800

    Use onle the Unqualified qualifier.

--- src/jillexample/ExampleChecker.java
+++ src/jillexample/ExampleChecker.java
@@ -1,22 +1,11 @@
 package jillexample;
 
 import checkers.quals.TypeQualifiers;
-import checkers.quals.TypeQualifier;
-import checkers.quals.SubtypeOf;
+import checkers.quals.Unqualified;
 
 import jill.InstrumentingChecker;
 
-// In this example, we're not using any type qualifiers -- just doing
-// instrumentation. Unfortunately, the Checker Framework requires us to
-// provide at least two annotations.
-@TypeQualifier
-@SubtypeOf({})
-@interface DummyQual1 { }
-@TypeQualifier
-@SubtypeOf({})
-@interface DummyQual2 { }
-
-@TypeQualifiers({DummyQual1.class, DummyQual2.class})
+@TypeQualifiers({Unqualified.class})
 public class ExampleChecker extends InstrumentingChecker {
     @Override
     public ExampleInstrumentor getInstrumentor() {
